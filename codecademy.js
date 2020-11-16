fe73fcee4c6d348042e2228713b379e5a2a81a97
Natalie East <eastnatalie1@gmail.com>
2020-11-15 23:16:01 -0600

    codecademy

--- codecademy.js
+++ codecademy.js
@@ -10,3 +10,9 @@ var farenheit = 68;
 fahrenheit = Math.floor(fahrenheit);
 //ensure fahrenheit rounds down to the nearest temperature
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+let newton = celsius * (33/100);
+//newton to celsius formula
+console.log(newton)
+newton = Math.floor(newton); //ensures temperature is rounded down
+console.log(`the temperature is ${newton} degrees Newton`);
