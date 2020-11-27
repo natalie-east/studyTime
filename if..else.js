0e793199a084bac0df64865ae20c4384cf1d5297
Natalie East <eastnatalie1@gmail.com>
2020-11-27 11:02:04 -0600

    codecademy project

--- if..else.js
+++ if..else.js
@@ -56,3 +56,58 @@ let favoritePhrase = 'Love That!';
 favoritePhrase === 'Love That!' ?
     console.log('I love that!'):
     console.log("I don't love that!");
+
+let userName = 'Natalie';
+userName = 'name' ? console.log ('Hello ' + userName + '!') : console.log('Hello!');
+
+let userQuestion = 'Who will I Marry?';
+switch (userQuestion){
+    case "Who will I Marry?":
+        console.log(userName + ', you will marry a life long friend.');
+        break;
+}
+
+let userName = 'Natalie';
+userName = 'name' ? console.log ('Hello ' + userName + '!') : console.log('Hello!');
+
+let userQuestion = 'Who will I Marry?';
+switch (userQuestion){
+    case "Who will I Marry?":
+        console.log(userName + ', you will marry a life long friend.');
+        break;
+}
+//formula the outputs a random number//
+let randomNumber = Math.floor(Math.random() * 8);
+//introduced a variable and set it equal to a number//
+let eightBall = 8;
+//write a switch statement for each number//
+switch (randomNumber){
+    case (0):
+        eightBall = 'It is certain';
+        break;
+    case (2):
+        eightBall = 'It is decidedly so';
+        break;
+    case (3):
+        eightBall = 'Reply is hazy, try again';
+        break;
+    case (4):
+        eightBall = 'Cannot predict now';
+        break;
+    case (5):
+        eightBall = 'Do not count on it';
+        break;
+    case (6):
+        eightBall = 'My sources say no';
+        break;
+    case (7):
+        eightBall = 'Outlook not so good';
+        break;
+    case (8):
+        eightBall = 'Signs point to yes';
+        break;
+}
+//statement expected output
+console.log('The Magic Eight Ball answered: ' + eightBall);
+
+
